21ae0b7520e237a261c70b4b5a39132570b1a765
tangyun <7418904+tangyuntangyun@user.noreply.gitee.com>
2025-02-26 00:22:02 +0800

    Add: Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Button from './components/Button/button';
+import './styles/index.scss'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +20,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+     <Button btnType="primary" size="lg"> primary button </Button>
     </div>
   );
 }
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,70 @@
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react'
+import classNames from 'classnames'
+
+export type ButtonSize = 'lg' | 'sm'
+export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
+
+interface BaseButtonProps {
+  className?: string;
+  /**设置 Button 的禁用 */
+  disabled?: boolean;
+  /**设置 Button 的尺寸 */
+  size?: ButtonSize;
+  /**设置 Button 的类型 */
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
+}
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
+// Partial<T>：是 TypeScript 的工具类型，将 T 中的所有属性变为可选（即每个属性都可以不传）
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+/**
+ * 页面中最常用的的按钮元素，适合于完成特定的交互，支持 HTML button 和 a 链接 的所有属性
+ * ### 引用方法
+ * 
+ * ```javascript
+ * import { Button } from 'funny-ui'
+ * ```
+ */
+export const Button: FC<ButtonProps> = (props) => {
+  const { 
+    btnType = 'default',
+    className,
+    disabled = false,
+    size,
+    children,
+    href,
+    ...restProps
+  } = props
+  // btn, btn-lg, btn-primary
+  // A simple JavaScript utility for conditionally joining classNames together.
+  const classes = classNames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (btnType === 'link') && disabled
+  })
+  if (btnType === 'link' && href ) {
+    return (
+      <a
+        className={classes}
+        href={href}
+        {...restProps}
+      >
+        {children}
+      </a>
+    )
+  } else {
+    return (
+      <button
+        className={classes}
+        disabled={disabled}
+        {...restProps}
+      >
+        {children}
+      </button>
+    )
+  }
+}
+
+export default Button;
